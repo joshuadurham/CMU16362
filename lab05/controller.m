40e70613671a47905ecf7f67e1d512eb8832b606
Dimitris Konstantinidis <dimgkonst@gmail.com>
2019-09-30 17:22:45 -0400

    Completed the basic implementation of feedback control

--- lab05/controller.m
+++ lab05/controller.m
@@ -5,21 +5,39 @@ classdef controller
     properties
        refPose
        acPose
+       Vref
+       invTwr
+       diff
+       rrp
+       tau
+       kx
+       ky
+       kth
     end
     
     methods
-        function obj = controller(refPose,acPose)
+        function obj = controller(refPose,acPose,Vref)
             %UNTITLED Construct an instance of this class
             %   Detailed explanation goes here
             obj.refPose = refPose;
             obj.acPose = acPose;
-            obj.rrp = acPose.bToA(refPose);
+            obj.Vref = Vref;
+            obj.invTwr = acPose.aToB();
+            obj.diff = refPose.getPoseVec() - acPose.getPoseVec();
+            obj.rrp = invTwr*refPose.getPoseVec();
+            obj.tau = 0.6;
+            obj.kx = 1/obj.tau;
+            obj.ky = 2/(obj.tau^2*abs(Vref));
+            obj.kth = 2/obj.tau;
             
         end
         
-        function [V, omega] = error(obj)
-            vl = interp1(obj.tSamples, obj.vlSamples, t);
-            vr = interp1(obj.tSamples, obj.vrSamples, t);
+        function [uV, uOmega] = error(obj)
+            ex = obj.rrp(1);
+            ey = obj.rrp(2);
+            eth = obj.diff(3);
+            uV = obj.kx*ex;
+            uOmega = obj.ky*ey+obj.kth*eth;
         end
         
     end
